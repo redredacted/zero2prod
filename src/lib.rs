c4fd2a4a8525f8aef63111aa3622869c5448993c
Kenny Sweet <redredacted@gmail.com>
2023-04-16 13:27:33 -0500

    - added a basic health check

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::net::TcpListener;
+
+use actix_web::{web, App, HttpServer, Responder, HttpResponse, dev::Server};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
