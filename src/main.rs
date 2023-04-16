2d461521784fc3612a799d2bf916ae0fc779c585
Kenny Sweet <redredacted@gmail.com>
2023-04-16 13:30:07 -0500

    - ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000")
-        .expect("Failed to to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to to bind random port");
     run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,10 +5,10 @@ async fn health_check_works() {
     // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
-    
+
     // Act
     let response = client
-        .get(&format!("{}/health_check", &address)) 
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,8 +19,7 @@ async fn health_check_works() {
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
