ff6b61d6757f62cd64b578886232b6e101941404
Kenny Sweet <redredacted@gmail.com>
2023-04-22 19:32:35 -0500

    - added sqlx and organized code more

---
+++ src/routes/mod.rs
@@ -0,0 +1,5 @@
+mod health_check;
+mod subscription;
+
+pub use health_check::*;
+pub use subscription::*;
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
@@ -30,6 +32,12 @@ async fn health_check_works() {
 async fn subscribe_returns_a_200_for_vaild_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration file.");
+    let connection_string = configuration.database.connection_string();
+    println!("{}", &connection_string);
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connecti to postgres");
     let client = reqwest::Client::new();
 
     // Act
@@ -44,7 +52,16 @@ async fn subscribe_returns_a_200_for_vaild_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection) 
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
+
 
 #[tokio::test]
 async fn subscribe_return_a_400_when_data_is_missing() {
